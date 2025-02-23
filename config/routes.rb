231f8c3867729218dd703525430553d8c6682c26
King'ori Maina <j@kingori.co>
2025-02-23 08:24:50 +0200

    Add homepage (redirects to show-bible page)

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  # Home
+  root to: "pages#home"
+
   # Articles
   resources :articles, param: :slug, only: [ :index, :show ]
 
---
+++ spec/requests/pages_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :request do
+  describe 'GET #home' do
+    it 'returns the correct response' do
+      get root_path
+
+      aggregate_failures do
+        expect(response).to have_http_status(:temporary_redirect)
+        expect(response.content_type).to eq 'text/html; charset=utf-8'
+      end
+    end
+  end
+end
