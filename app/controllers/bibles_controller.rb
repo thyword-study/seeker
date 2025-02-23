4aba24f8b1df00376d0b077e9d25740f68e90f9f
King'ori Maina <j@kingori.co>
2025-02-23 15:00:40 +0200

    Update show-bible (redirects to list-bible-books page)

--- app/controllers/bibles_controller.rb
+++ app/controllers/bibles_controller.rb
@@ -1,5 +1,8 @@
 class BiblesController < ApplicationController
   def show
-    render html: ""
+    bible_code = params[:code].to_s.strip.upcase
+    bible = Bible.find_by! code: bible_code
+
+    redirect_to bible_books_url(bible_code: bible.code.downcase), status: :temporary_redirect
   end
 end
