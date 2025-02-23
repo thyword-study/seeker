8c6cd6b40fdbf691fa9da54228b6ac278ef97d3a
King'ori Maina <j@kingori.co>
2025-02-23 15:59:51 +0200

    Update list-book-chapters page

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,6 +1,11 @@
 class ChaptersController < ApplicationController
   def index
-    render html: ""
+    bible_code = params[:bible_code].to_s.strip.upcase
+    book_slug = params[:book_slug].to_s.strip
+
+    @bible = Bible.find_by! code: bible_code
+    @book = Book.find_by! bible: @bible, slug: book_slug
+    @chapters = Chapter.where(bible: @bible, book: @book).order(number: :asc)
   end
 
   def show
