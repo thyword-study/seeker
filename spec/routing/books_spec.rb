dc2d95e633b6ae96155ee3b46d8a37ce0c2c3cc0
King'ori Maina <j@kingori.co>
2025-02-23 16:21:16 +0200

    Prefer to force bible code to lowercase in URLs

--- spec/routing/books_spec.rb
+++ spec/routing/books_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :routing do
   describe 'routing' do
     it 'routes GET /bibles/:bible_code/books to #index' do
-      expect(get: '/bibles/BSB/books').to route_to('books#index', bible_code: 'BSB')
+      expect(get: '/bibles/bsb/books').to route_to('books#index', bible_code: 'bsb')
     end
 
     it 'routes GET /bibles/:bible_code/books/:slug to #show' do
-      expect(get: '/bibles/BSB/books/genesis').to route_to('books#show', bible_code: 'BSB', slug: 'genesis')
+      expect(get: '/bibles/bsb/books/genesis').to route_to('books#show', bible_code: 'bsb', slug: 'genesis')
     end
   end
 end
--- spec/routing/chapters_spec.rb
+++ spec/routing/chapters_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe ChaptersController, type: :routing do
   describe 'routing' do
     it 'routes GET /bibles/:bible_code/books/:book_slug/chapters to #index' do
-      expect(get: '/bibles/BSB/books/genesis/chapters').to route_to('chapters#index', bible_code: 'BSB', book_slug: 'genesis')
+      expect(get: '/bibles/bsb/books/genesis/chapters').to route_to('chapters#index', bible_code: 'bsb', book_slug: 'genesis')
     end
 
     it 'routes GET /bibles/:bible_code/books/:book_slug/chapters/:number to #show' do
-      expect(get: '/bibles/BSB/books/genesis/chapters/1').to route_to('chapters#show', bible_code: 'BSB', book_slug: 'genesis', number: '1')
+      expect(get: '/bibles/bsb/books/genesis/chapters/1').to route_to('chapters#show', bible_code: 'bsb', book_slug: 'genesis', number: '1')
     end
   end
 end
