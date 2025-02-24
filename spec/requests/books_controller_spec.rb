e9a7f71113daf1af14d2d7cc58e30b6dfa8af0d8
King'ori Maina <j@kingori.co>
2025-02-24 06:28:44 +0200

    Update tests to handle lower-case bible code

--- spec/requests/books_controller_spec.rb
+++ spec/requests/books_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :request do
   describe 'GET #index' do
     it 'returns the correct response' do
-      get bible_books_path bible_code: 'BSB'
+      get bible_books_path bible_code: 'bsb'
 
       aggregate_failures do
         expect(response).to have_http_status(:ok)
@@ -14,10 +14,10 @@ RSpec.describe BooksController, type: :request do
 
   describe 'GET #show' do
     it 'returns the correct response' do
-      get bible_book_path bible_code: 'BSB', slug: 'genesis'
+      get bible_book_path bible_code: 'bsb', slug: 'genesis'
 
       aggregate_failures do
-        expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:temporary_redirect)
         expect(response.content_type).to eq 'text/html; charset=utf-8'
       end
     end
--- spec/requests/chapters_controller_spec.rb
+++ spec/requests/chapters_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :request do
   describe 'GET #index' do
     it 'returns the correct response' do
-      get bible_book_chapters_path bible_code: 'BSB', book_slug: "genesis"
+      get bible_book_chapters_path bible_code: 'bsb', book_slug: "genesis"
 
       aggregate_failures do
         expect(response).to have_http_status(:ok)
@@ -14,7 +14,7 @@ RSpec.describe BooksController, type: :request do
 
   describe 'GET #show' do
     it 'returns the correct response' do
-      get bible_book_chapter_path bible_code: 'BSB', book_slug: 'genesis', number: "1"
+      get bible_book_chapter_path bible_code: 'bsb', book_slug: 'genesis', number: "1"
 
       aggregate_failures do
         expect(response).to have_http_status(:ok)
