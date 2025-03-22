f7f435afb9cf58da44ec5a4ced0e284e2c39dbf6
King'ori Maina <j@kingori.co>
2025-03-22 14:19:27 +0200

    Redirect show-book page to list-chapters

--- app/controllers/commentary/books_controller.rb
+++ app/controllers/commentary/books_controller.rb
@@ -6,7 +6,12 @@ module Commentary
     end
 
     def show
-      render html: ""
+      book_slug = params[:slug].to_s.strip
+
+      @bible = Bible.find_by! code: Settings.bible.defaults.translation
+      @book = Book.find_by! bible: @bible, slug: book_slug
+
+      redirect_to commentary_book_chapters_path(book_slug: @book.slug), status: :temporary_redirect
     end
   end
 end
--- spec/requests/commentary/books_controller_spec.rb
+++ spec/requests/commentary/books_controller_spec.rb
@@ -17,10 +17,13 @@ module Commentary
 
     describe 'GET #show' do
       it 'returns the correct response' do
-        get commentary_book_path slug: 'genesis'
+        bible = FactoryBot.create(:bible_bsb)
+        book = FactoryBot.create(:book, bible: bible)
+
+        get commentary_book_path slug: book.slug
 
         aggregate_failures do
-          expect(response).to have_http_status(:ok)
+          expect(response).to have_http_status(:temporary_redirect)
           expect(response.content_type).to eq 'text/html; charset=utf-8'
         end
       end
