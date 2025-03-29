85e7e5262af4b1ded2c9fb2feff477445bf28545
King'ori Maina <j@kingori.co>
2025-03-29 14:59:57 +0200

    Remove seeding before tests

--- spec/support/database_cleaner_active_record.rb
+++ spec/support/database_cleaner_active_record.rb
@@ -2,8 +2,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
-
-    Rails.application.load_seed
   end
 
   config.around do |example|
