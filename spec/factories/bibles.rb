8d0784fa475673bdace40a07458af8d99d232bf6
King'ori Maina <j@kingori.co>
2025-03-29 21:48:50 +0200

    Add basic factories for easier testing

--- spec/factories/bibles.rb
+++ spec/factories/bibles.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :bible do
-    name { Faker::Lorem.word.titlecase }
     code { (0...3).map { ('a'..'z').to_a.sample }.join.upcase }
+    name { Faker::Lorem.word.titlecase }
     rights_holder_name { Faker::Company.name }
     rights_holder_url { Faker::Internet.url }
     statement { Faker::Lorem.sentence }
   end
 
   factory :bible_bsb, parent: :bible do
-    name { "Berean Standard Bible" }
     code { "BSB" }
+    name { "Berean Standard Bible" }
   end
 end
