ab5dde4e4b7ea92821831e73244acf5c0e02b1cc
King'ori Maina <j@kingori.co>
2025-03-30 14:37:50 +0200

    Configure vcr to hide authorization header

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,4 +1,15 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
+
+  config.filter_sensitive_data('<TOKEN>') do |interaction|
+    headers = interaction.request.headers
+
+    if headers['Authorization'] && headers['Authorization'].first
+      auths = headers['Authorization'].first
+      if (match = auths.match(/^Bearer\s+([^,\s]+)/))
+        match.captures.first
+      end
+    end
+  end
 end
