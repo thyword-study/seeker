864df5bcbf8d2529986b3dac4a4a4b08a0a7b9ce
King'ori Maina <j@kingori.co>
2025-04-04 19:28:08 +0200

    Configure VCR to redact Openai-Organization request header

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,12 +2,23 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
 
-  config.filter_sensitive_data('<TOKEN>') do |interaction|
+  config.filter_sensitive_data('[REDACTED]') do |interaction|
     headers = interaction.request.headers
 
     if headers['Authorization'] && headers['Authorization'].first
       auths = headers['Authorization'].first
       if (match = auths.match(/^Bearer\s+([^,\s]+)/))
+        match.captures.first
+      end
+    end
+  end
+
+  config.filter_sensitive_data('[REDACTED]') do |interaction|
+    headers = interaction.request.headers
+
+    if headers['Openai-Organization'] && headers['Openai-Organization'].first
+      orgs = headers['Openai-Organization'].first
+      if (match = orgs.match(/^org-([^,\s]+)/))
         match.captures.first
       end
     end
