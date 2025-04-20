cc237bf40eea9e9da770895888ad246d57e87f50
King'ori Maina <j@kingori.co>
2025-04-20 13:48:13 +0200

    Add Exposit::FetchBatchRequestContentsJob job

---
+++ spec/jobs/exposit/fetch_batch_request_contents_job_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Exposit::FetchBatchRequestContentsJob, type: :job do
+  describe "#perform" do
+    it "fetches the batch request contents without errors"
+  end
+end
