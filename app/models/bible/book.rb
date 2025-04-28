11323cc36c1261d0d351f9a415b7ee08f1b94e5d
King'ori Maina <j@kingori.co>
2025-04-28 16:58:02 +0200

    Add Book#next

--- app/models/bible/book.rb
+++ app/models/bible/book.rb
@@ -57,4 +57,18 @@ class Bible::Book < ApplicationRecord
   # Scopes
   scope :old_testament, -> { where(testament: "OT") }
   scope :new_testament, -> { where(testament: "NT") }
+
+  # Returns the number of the next book in the translation, or `nil` if the
+  # current book is the last one in the translation.
+  #
+  # @return [Integer, nil] The number of the next book, or `nil` if there is no
+  #   next book.
+  def next_number
+    if number == translation.books_count
+      nil
+    else
+      number + 1
+    end
+  end
+
 end
